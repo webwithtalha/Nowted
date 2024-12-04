094e5acc2d9c4faa14f0bae658f78f1b9e212475
Talha zulfiqar <csharpjava0@gmail.com>
2024-12-04 19:17:53 +0000

    Simplify AuthPage component by removing SignUpCard and search parameter handling

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import SignInCard from "./SignIn";
-import SignUpCard from "./SignUp/page";
 
 const AuthPage = () => {
-  const searchParams = useSearchParams();
-  const mode = searchParams.get("mode") || "sign-in";
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-customBlack">
-      {mode === "sign-in" ? <SignInCard /> : <SignUpCard />}
+      <SignInCard />
     </div>
   );
 };
